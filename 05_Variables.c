2d2e8b8b96319a4dd4cf76b2b3d24ed00840a975
Abhishek Choudhary <abhishekghosalya33@gmail.com>
2025-10-06 14:34:03 +0530

    initial commit

---
+++ 05_Variables.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    int a =10;
+    int b=a;
+    int c=12,d=23,e=65;
+
+    printf("the value of a %d and value of b is %d\n",a,b);
+    printf("the value of c %d and value of d is %d\n",c,d);
+    printf("the value of e %d",e);
+    return 0;
+}
