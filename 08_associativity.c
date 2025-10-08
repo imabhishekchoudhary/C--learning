9a04a3ba68584a172679b90e46010dcda16982cb
Abhishek Choudhary <abhishekghosalya33@gmail.com>
2025-10-08 21:59:47 +0530

    initial commit

---
+++ 08_associativity.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    int a = 5;
+    int b = 6;
+    int c = 2;
+
+    printf("%d\n",a*b/c);
+    printf("%d\n",2*b/c*a+33);
+    printf("%d",2*b/c*a+7*2);
+    return 0;
+
+}
