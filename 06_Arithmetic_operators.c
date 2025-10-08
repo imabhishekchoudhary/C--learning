7d5e80996255cd747341c7303e6c6afa03de3764
Abhishek Choudhary <abhishekghosalya33@gmail.com>
2025-10-08 21:57:37 +0530

    initial commit

---
+++ 06_Arithmetic_operators.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+    int a=3;
+    int b=2;
+    int c=a+b;
+
+    printf("the value of a is %d and the value of b is %d\n and sum is %d\n",a,b,c);
+    printf("the remender  a and b is:%d",a%b);
+    return 0;
+}
