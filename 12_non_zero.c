531aba64dad00352644112cd5fc201a5148f8e7e
Abhishek Choudhary <abhishekghosalya33@gmail.com>
2025-10-10 21:50:24 +0530

    initial commit

---
+++ 12_non_zero.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+    if(1){
+        printf("this is the executed\n");
+    }
+     if(33.3){
+        printf("this is the executed\n");
+    }
+     if('a'){
+        printf("this is the executed\n");
+    }
+     if(0){
+        printf("this is the executed\n");
+    }
+}
