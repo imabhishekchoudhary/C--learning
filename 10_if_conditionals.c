3172386dc5996672ba653988908675b0c1a6c99c
Abhishek Choudhary <abhishekghosalya33@gmail.com>
2025-10-10 21:49:12 +0530

    initial commit

---
+++ 10_if_conditionals.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+    
+    int age = 20;
+    
+    if(age>18){
+        printf("you are the right in this age\n");
+    }
+
+    if(age<22){
+        printf("you are the right.");
+    }
+    return 0;
+}
