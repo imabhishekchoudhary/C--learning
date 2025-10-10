304fbd7249e7551bfa8103570c3f1ec4ff3441d7
Abhishek Choudhary <abhishekghosalya33@gmail.com>
2025-10-10 21:49:48 +0530

    initial commit

---
+++ 11_if_else_.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+    int age =18;
+
+    //int age;
+   // printf("enter your age");
+   // scanf("%d",&age);
+
+    if(age>20){
+        printf("we are inside if");
+    }
+
+    else{
+        printf("your age is not greater then 20");
+    }
+}
