888b154a3bd3c74ef53b1abdc4966059b09deadf
Abhishek Choudhary <abhishekghosalya33@gmail.com>
2025-11-30 20:25:01 +0530

    initial commit

---
+++ 16_switch_case.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+    int a;
+    printf("Enter the value of a :");
+    scanf("%d",&a);
+
+    switch (a){
+        case 1:
+            printf("you enterd 1\n");
+            break;
+
+         case 2:
+            printf("you enterd 2\n");
+            break;
+            
+        case 3:
+            printf("you enterd 3\n");
+            break;
+            
+         case 4:
+            printf("you enterd 4\n");
+            break;           
+    }
+}
