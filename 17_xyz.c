c54da34f03cf8a65ed0586cde6333ea930ebcdef
Abhishek Choudhary <abhishekghosalya33@gmail.com>
2025-11-30 20:25:53 +0530

    initial commit

---
+++ 17_xyz.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+    int class=12;
+    
+    if(class>=11){
+        printf("your subject's is\n Physics,Chemistery,math's");
+    }
+    
+    else if(class>=10){
+        printf("your subjrct's is\n Hindi,English,math,science,social science,sanskarth");
+    }
+
+}
